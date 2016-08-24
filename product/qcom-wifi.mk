c99e5db07572b389a521e29722af786143f58c09
Teo Ren Jie <boylush@outlook.com>
2016-08-24 16:12:23 +0800

    Shift wlan blobs from vendor
    Signed-off-by: Teo Ren Jie <boylush@outlook.com>

--- product/qcom-wifi.mk
+++ product/qcom-wifi.mk
@@ -1,4 +1,14 @@
 # Qualcom WiFi
 PRODUCT_COPY_FILES += \
     device/sony/tulip/wifi/wpa_supplicant_overlay.conf:system/etc/wifi/wpa_supplicant_overlay.conf \
-    device/sony/tulip/wifi/p2p_supplicant_overlay.conf:system/etc/wifi/p2p_supplicant_overlay.conf
+    device/sony/tulip/wifi/p2p_supplicant_overlay.conf:system/etc/wifi/p2p_supplicant_overlay.conf \
+	device/sony/tulip/wifi/WCNSS_qcom_wlan_nv.bin:system/etc/firmware/wlan/prima/WCNSS_qcom_wlan_nv.bin \
+    device/sony/tulip/wifi/WCNSS_cfg.dat:system/etc/firmware/wlan/prima/WCNSS_cfg.dat \
+	device/sony/tulip/wifi/WCNSS_qcom_cfg.ini:system/etc/wifi/WCNSS_qcom_cfg.ini
+	
+# Wifi
+PRODUCT_PACKAGES += \
+    wpa_supplicant \
+    wpa_supplicant.conf \
+    wpa_supplicant_overlay.conf \
+    p2p_supplicant_overlay.conf
