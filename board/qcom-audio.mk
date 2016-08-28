2e412cebe1dcfee1328357c17e5e46271725bc34
Teo Ren Jie <boylush@outlook.com>
2016-08-28 18:35:46 +0800

    Fix screw up (audio board)
    Signed-off-by: Teo Ren Jie <boylush@outlook.com>

--- board/qcom-audio.mk
+++ board/qcom-audio.mk
@@ -1,5 +1,3 @@
-# BT definitions for Qualcomm solution
-BLUETOOTH_HCI_USE_MCT := true
-BOARD_BLUETOOTH_BDROID_BUILDCFG_INCLUDE_DIR := device/sony/tulip/bluetooth
-BOARD_HAVE_BLUETOOTH := true
-BOARD_HAVE_BLUETOOTH_QCOM := true
+# Audio
+BOARD_USES_ALSA_AUDIO := true
+AUDIO_FEATURE_ENABLED_MULTI_VOICE_SESSIONS := true
