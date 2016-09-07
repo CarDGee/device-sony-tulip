a3f844801db571c970e88f8aa9b9a503dfdf26fe
cardgee <carlosdgguerreiro@gmail.com>
2016-09-07 19:52:25 +0000

    Initial CM Bringup

---
+++ product/audio.mk
@@ -0,0 +1,4 @@
+# Media etc
+PRODUCT_COPY_FILES := \
+    device/sony/tulip/proprietary/etc/mixer_paths.xml:system/etc/mixer_paths.xml \
+    device/sony/tulip/proprietary/etc/mixer_paths_mtp.xml:system/etc/mixer_paths_mtp.xml    
